a0baf8f45ace8872027975f14fce09037c5875f2
Ameminn-python <63637891+Ameminn-python@users.noreply.github.com>
2020-04-16 20:14:33 +0900

    Update ActionCog.py

--- ActionCog.py
+++ ActionCog.py
@@ -4,3 +4,10 @@ from discord.ext import commands
 class actioncommand(commands.Cog):
     
     def __init__(self):
+        pass
+        
+    
+    @commands.command()
+    async def hello(ctx):
+        await ctx.send('hello')
+    
