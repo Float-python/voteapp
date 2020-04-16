8f9b3bfff7cdbde575294ae2903e2c3011211e1e
Ameminn-python <63637891+Ameminn-python@users.noreply.github.com>
2020-04-16 19:05:42 +0900

    UPDATE general help

--- HelpCog.py
+++ HelpCog.py
@@ -10,6 +10,7 @@ class helpcommands(commands.Cog):
         if feature is None:
             help_feature = discord.Embed(title='Help commands help :D',description='*help <**feature**>',colour=discord.Colour.orange())
             help_feature.add_field(name='**vote**',value='Vote feature help use this *help vote',inline=False)
+            help_feature.add_field(name='**general**',value="this bot's general cmd help",inline=False)
             await ctx.send(embed=help_feature)
         elif feature == 'vote':
             help_vote = discord.Embed(title='Vote Help',description='if you want check other feature pls Type *help',colour=discord.Colour.orange())
@@ -17,6 +18,11 @@ class helpcommands(commands.Cog):
             help_vote.add_field(name='② *asr <asr1>/<asr2>',value='Disignation answer (split**/**)※Max4answer',inline=False)
             help_vote.add_field(name='③ *fin',value='finish questionnaire',inline=False)
             await ctx.send(embed=help_vote)
+        elif feature == 'general':
+            help_general = discord.Embed(title='General Help',description='this bot general command',colour=discord.Colour.orange())
+            help_general.add_field(name='*code*'value='if you use this command, you can get source code',inline=False)
+            help_general.add_field(name='*invite*',value='return this bot invite URL',inline=False)
+            await ctx.send(embed=help_general)
         else:
             await ctx.send('can not found this feature pls chack *help .')
                                          
