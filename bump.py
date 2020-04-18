6db81d8bb938f2008a0e9081a214d48d5c8bc0b5
Ameminn-python <63637891+Ameminn-python@users.noreply.github.com>
2020-04-18 17:29:53 +0900

    Update bump.py

--- bump.py
+++ bump.py
@@ -1,1 +1,23 @@
+from discord.ext import commands
+import discord
+import asyncio
 
+class Bump(commands.Cog):
+    
+    def __init__(self,bot):
+        self.bot = bot
+        self.bump_time = dict()
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        official_server = self.bot.get_guild(700603441302732873)
+        
+        b_dict = dscord.Embed(title='botが再起動しました!',description='Bumpに関する辞書です',colour=discord.Colour.blue())
+        for g in self.bot.guilds:
+            self.bump[g.id] = None
+            b_dict.add_field(name='**辞書サーバー概要**',value=str(g.name)+'\n'+str(g.id))
+            await 
+            
+            
+            
+    
